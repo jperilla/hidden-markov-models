3f0fe19d2fc55feae5accaf7cd4c3bec8347369e
jperilla <julie.perilla@gmail.com>
2018-10-21 09:19:00 -0600

    Initialize matrices

--- viterbi.py
+++ viterbi.py
@@ -13,11 +13,22 @@ Input:
 Output:
     v = Likely series of states that occurred to produce the observed activities.
 """
-import numpy
+import numpy as np
 
 
 def viterbi(q, s, a, pi, e, y):
-    v = numpy.empty((3, 3,))
+    k = len(q)
+    n = len(s)
+    v = np.zeros((k, n,))
     return v
 
 
+Q = ['Rainy', 'Sunny']  # States
+S = ['WALK', 'SHOP', 'CLEAN']  # Possible events
+A = np.array([[.7, .3], [.4, .6]])  # Transition Matrix
+PI = [0.6, 0.4] # Start probabilities
+E = np.array([[.1, .4, .5], [.6, .3, .1]])  # Emission Matrix
+Y = ['WALK', 'SHOP', 'CLEAN']  # Actual events
+
+V = viterbi(Q, S, A, PI, E, Y)
+print(V)
